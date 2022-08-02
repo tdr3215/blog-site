20e0981bd53ca89e920f41ca1b79cadf6f5aeb32
Tiffany Rose <tiffany.drose@gmail.com>
2022-08-02 11:35:34 -0400

    first commit

---
+++ src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
@@ -0,0 +1,88 @@
+package com.techtalentsouth.TechTalentBlog.BlogPost;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BlogPostController {
+
+	@Autowired
+	private BlogPostRepository blogPostRepository;
+//	private static List<BlogPost> posts = new ArrayList<>();
+
+	@GetMapping(value = "/")
+	public String index(BlogPost blogPost, Model model) {
+		model.addAttribute("posts", blogPostRepository.findAll());
+		return "blogpost/index";
+	}
+
+	@GetMapping(value = "/blogposts/{id}")
+	public String viewPostById(@PathVariable Long id, BlogPost blogpost, Model model) {
+		Optional<BlogPost> post = blogPostRepository.findById(id);
+		if (post.isPresent()) {
+			BlogPost actualPost = post.get();
+			model.addAttribute("blogPost", actualPost);
+		}
+
+		return "blogpost/result";
+	}
+
+	@GetMapping(value = "/blogposts/new")
+	public String newBlog(BlogPost blogPost) {
+		return "blogpost/new";
+	}
+
+	@PostMapping(value = "/blogposts")
+	public String addNewBlogPost(BlogPost blogPost, Model model) {
+		BlogPost savedBlogPost = blogPostRepository
+				.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+		model.addAttribute("blogPost", savedBlogPost);
+		// TODO: Build the single blogpost page, and redirect there instead.
+		return "blogpost/result";
+	}
+
+	// TODO: GET Route to display the Edit page
+	@GetMapping(value = "/blogposts/{id}/edit")
+	public String editBlog(@PathVariable Long id, BlogPost blogPost, Model model) {
+		Optional<BlogPost> post = blogPostRepository.findById(id);
+		if (post.isPresent()) {
+			BlogPost actualPost = post.get();
+			model.addAttribute("blogPost", actualPost);
+		}
+		return "blogpost/edit";
+	}
+
+	// TODO: PUT Route to actually edit the page from the Edit page form submission
+
+	@RequestMapping(value = "/blogposts/{id}")
+	public String updateBlogPost(@PathVariable Long id, BlogPost blogPost, Model model) {
+		Optional<BlogPost> post = blogPostRepository.findById(id);
+		if (post.isPresent()) {
+			BlogPost actualPost = post.get();
+			actualPost.setTitle(blogPost.getTitle());
+			actualPost.setAuthor(blogPost.getAuthor());
+			actualPost.setBlogEntry(blogPost.getBlogEntry());
+			blogPostRepository.save(actualPost);
+			model.addAttribute("blogPost", actualPost);
+		}
+		return "blogpost/result";
+	}
+
+	@RequestMapping(value = "/blogposts/{id}", method = RequestMethod.DELETE)
+	public String deletePostWithId(@PathVariable Long id, BlogPost blogPost) {
+		blogPostRepository.deleteById(id);
+		return "redirect:/";
+	}
+}
+
+
